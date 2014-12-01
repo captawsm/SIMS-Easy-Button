686d4cbca77f3632e10fa31af6e00aa8e6df4298
captawsm <j.thomas.roberson@gmail.com>
2014-11-30 21:40:54 -0600

    update to 0.1.1

--- Chrome/background.js
+++ Chrome/background.js
@@ -5,25 +5,30 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
 		
 		//places the matched part of the URL in baseURL[0]
 		var baseURL = re.exec(tabs[0].url);
-	
-		//take baseURL[0] and concatenates the necessary subdirectories
-		var SSID = baseURL[0] + "netconfig_wifi.php";
-		var onlineDevices = baseURL[0] + "identify.php";
-		var landingPage = baseURL[0] + "proxy_disclaimer.php";
-		var roomBinding = baseURL[0] + "roomlist.php";
-		var disclaimerLogs = baseURL[0] + "wifistats_disclaimers.php";
-		var hsiaSubscription = baseURL[0] + "subscr_audit.php";
-		var fullReport = baseURL[0] + "sysstatus_full.php";
 		
-		//open new tab in background with the new URL
-		chrome.tabs.create({ url: onlineDevices, active: false });
-		chrome.tabs.create({ url: landingPage, active: false });
-		chrome.tabs.create({ url: roomBinding, active: false });
-		chrome.tabs.create({ url: disclaimerLogs, active: false });
-		chrome.tabs.create({ url: hsiaSubscription, active: false });
-		chrome.tabs.create({ url: fullReport, active: false });
-		
-		//navigate the current selected tab to a new page
-		chrome.tabs.update(tabs[0].id, {url: SSID});
+		//check to make sure there was a match
+		//further research indicates if(baseURL) or if(baseURL !== null) should be used instead
+		//will test and decide
+		if(baseURL != null) {
+			//take baseURL[0] and concatenates the necessary subdirectories
+			var SSID = baseURL[0] + "netconfig_wifi.php";
+			var onlineDevices = baseURL[0] + "identify.php";
+			var landingPage = baseURL[0] + "proxy_disclaimer.php";
+			var roomBinding = baseURL[0] + "roomlist.php";
+			var disclaimerLogs = baseURL[0] + "wifistats_disclaimers.php";
+			var hsiaSubscription = baseURL[0] + "subscr_audit.php";
+			var fullReport = baseURL[0] + "sysstatus_full.php";
+				
+			//open new tab in background with the new URL
+			chrome.tabs.create({ url: onlineDevices, active: false });
+			chrome.tabs.create({ url: landingPage, active: false });
+			chrome.tabs.create({ url: roomBinding, active: false });
+			chrome.tabs.create({ url: disclaimerLogs, active: false });
+			chrome.tabs.create({ url: hsiaSubscription, active: false });
+			chrome.tabs.create({ url: fullReport, active: false });
+				
+			//navigate the current selected tab to a new page
+			chrome.tabs.update(tabs[0].id, {url: SSID});
+		}
 	});
 });
