7e8e01d554ffa6302f9f5472b6957ce4cbcf92c5
captawsm <j.thomas.roberson@gmail.com>
2015-03-11 17:29:19 -0500

    0.3.0 updates

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,5 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(activeTab) {
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+chrome.browserAction.onClicked.addListener(function(activeTab) { //run when button click
+	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) { //get URL of current tab
 		//regexp that matches the first part of the URL
 		var re = new RegExp("https:\\/\\/.*?\\/htadmin\\/");
 		
@@ -9,10 +9,13 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
 		//check to make sure there was a match
 		//further research indicates if(baseURL) or if(baseURL !== null) should be used instead
 		//will test and decide
+		//decided to use if(baseURL) since if there's something there it should be "truthy"
 		if(baseURL) {
 			chrome.storage.sync.get(
+			//must be updated to add new tabs
 			//have to use key/value pairs instead of list of keys since defaults not otherwise set unless options is opened first
-			//example: ["tab1", "tab2", "tab3", "tab4", "tab5", "tab6", "tab7"]
+			//investigate if default value option can be moved outside of the ...addListener block to decrease code complexity/redundancy
+			//test block: ["tab1", "tab2", "tab3", "tab4", "tab5", "tab6", "tab7", "tab8", "tab9", "tab10"], function(items) {
 			{
 				"tab1": "netconfig_wifi.php",
 				"tab2": "identify.php",
@@ -26,17 +29,20 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
 				"tab10": ""
 			}, function(items) {
 				//open new tab in background with the new URL
-				
-				//this is shit but i don't know how to iterate through an objects properties
-				var loadTabs = [items.tab1, items.tab2, items.tab3, items.tab4, items.tab5, items.tab6, items.tab7, items.tab8, items.tab9, items.tab10];
+				//this is shit but i don't know how to iterate through an objects properties [actually can use "for(var in object)"]
+				//var loadTabs = [items.tab1, items.tab2, items.tab3, items.tab4, items.tab5, items.tab6, items.tab7, items.tab8, items.tab9, items.tab10];
 				var firstTab = true;
-				for(i = 0; i < loadTabs.length; i++) {
-					if(loadTabs[i]) {
+				for(var key in items) { //just testing [seems to work actually!]
+				//for(i = 0; i < loadTabs.length; i++) {
+					if(items[key]) {
+					//if(loadTabs[i]) {
 						if(firstTab) {
-							chrome.tabs.update(tabs[0].id, {url: baseURL[0] + loadTabs[i]});
+							chrome.tabs.update(tabs[0].id, {url: baseURL[0] + items[key]}); //seems to work
+							//chrome.tabs.update(tabs[0].id, {url: baseURL[0] + loadTabs[i]});							
 							firstTab = false;
 						} else {
-							chrome.tabs.create({ url: baseURL[0] + loadTabs[i], active: false });
+							chrome.tabs.create({url: baseURL[0] + items[key], active: false }); //seems to work
+							//chrome.tabs.create({ url: baseURL[0] + loadTabs[i], active: false });
 						}
 					}
 				}
--- Chrome/options.js
+++ Chrome/options.js
@@ -1,5 +1,6 @@
 ﻿// Saves options to chrome.storage
 function save_options() {
+//must be updated to add more tabs
   var tab1 = document.getElementById('tab1').value;
   var tab2 = document.getElementById('tab2').value;
   var tab3 = document.getElementById('tab3').value;
@@ -10,6 +11,7 @@ function save_options() {
   var tab8 = document.getElementById('tab8').value;
   var tab9 = document.getElementById('tab9').value;
   var tab10 = document.getElementById('tab10').value;
+  //must be updated to add more tabs
   chrome.storage.sync.set({
     "tab1": tab1,
 	"tab2": tab2,
@@ -47,8 +49,11 @@ function reset_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  //default
+  //default settings
+  //must be updated to add more tabs
   chrome.storage.sync.get({
+	//default values specified here too just in case someone opens options before using extension
+	//should check if there is a "on install" script instead, maybe in background.js but move it outside of "addListener" block
     "tab1": "netconfig_wifi.php",
 	"tab2": "identify.php",
 	"tab3": "proxy_disclaimer.php",
@@ -59,7 +64,7 @@ function restore_options() {
 	"tab8": "",
 	"tab9": "",
 	"tab10": ""
-  }, function(items) {
+  }, function(items) { must be updated to add new tabs
     document.getElementById('tab1').value = items.tab1;
     document.getElementById('tab2').value = items.tab2;
 	document.getElementById('tab3').value = items.tab3;
@@ -72,6 +77,40 @@ function restore_options() {
 	document.getElementById('tab10').value = items.tab10;
   });
 }
+//must be updated to add new tabs
+var tab_options = {
+	"Not Set": "",
+	"Wi-Fi Access Points": "netconfig_wifi.php",
+	"DHCP/Real IP": "netconfig_dhcp.php?mode=realip",
+	"SMTP": "netconfig_smtp.php",
+	"MAC-Wg": "netconfig_macwg.php",
+	"Online Devices": "identify.php",
+	"Disclaimer Pages": "proxy_disclaimer.php",
+	"Room/Port Binding": "roomlist.php",
+	"Users and PINs": "radius_users.php",
+	"Wi-Fi Disclaimer Logs": "wifistats_disclaimers.php",
+	"Wired Disclaimer Logs": "vbnstats_disclaimers.php",
+	"Wi-Fi Sessions": "wifistats_sessions.php",
+	"Wired Sessions": "vbnstats_sessions.php",
+	"HSIA Subscriptions": "subscr_audit.php",
+	"Full Report": "sysstatus_full.php",
+	"System Logs": "logs_system.php",
+	"Ping Tool": "ping.php"
+}
+//must be updated to add new tabs
+var total_tabs = 10;
+var select_holder = '';
+for(var i = 1; i <= total_tabs; i++) {
+	select_holder += ('<p>Tab '+i+'<br><select id="tab'+i+'">');
+	for(var key in tab_options) {
+		select_holder += ('<option value="'+tab_options[key]+'">'+key+'</option>');
+	}
+	select_holder += '</select></p>';
+}
+//too lazy to figure out how to do this with plain javascript - better include jQuery LOL!
+//can't seem to use .appendTo in the above nested for loops - it breaks it somehow
+$(select_holder).appendTo('#tab_list');
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
